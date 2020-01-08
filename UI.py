221a238d1c6a95b386d5cb871bc99a562133c3da
Subham02803 <subhambiswas688@gmail.com>
2020-01-08 06:24:08 +0530

    p

--- UI.py
+++ UI.py
@@ -7,9 +7,10 @@ from tensorflow.keras.models import load_model
 
 model = load_model('model.h5')
 
-
 root = tk.Tk()
 canvas = tk.Canvas(root, height=500, width=800, bg="#e6e6ff")
 canvas.pack()
+
+
 
 root.mainloop()
