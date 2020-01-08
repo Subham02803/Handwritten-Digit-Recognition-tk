dc6edd076706758c88a77391774e01757c71aab9
Subham02803 <subhambiswas688@gmail.com>
2020-01-08 07:01:46 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -2,15 +2,26 @@ import tkinter as tk
 import win32gui
 from PIL import ImageGrab, Image
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+#import tensorflow as tf
+#from tensorflow.keras.models import load_model
 
-model = load_model('model.h5')
+#model = load_model('model.h5')
 
 root = tk.Tk()
-canvas = tk.Canvas(root, height=500, width=800, bg="#e6e6ff")
+canvas = tk.Canvas(root, height=500, width=800, bg="#921ef7")
 canvas.pack()
 
+label1 = tk.Label(root, text="Draw a number", font=("Helvetica", 28))
+label1.place(relx=0.02, rely=0.04, relwidth=0.5, relheight=0.9)
+
+label2 = tk.Label(root, text="Thinking..", font=("Helvetica", 28))
+label2.place(relx=0.54, rely=0.44, relwidth=0.43, relheight=0.5)
+
+but_clr = tk.Button(root, text="Clear", font=("Helvetica", 18))
+but_clr.place(relx=0.54, rely=0.08, relwidth=0.17, relheight=0.07)
+
+but_rec = tk.Button(root, text="Recognise", font=("Helvetica", 18))
+but_rec.place(relx=0.54, rely=0.17, relwidth=0.17, relheight=0.07)
 
 
 root.mainloop()
